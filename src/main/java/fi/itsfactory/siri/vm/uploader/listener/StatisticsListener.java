d0d925fc3d7e6acd0f313529a45f43ab596b0ab1
juha.lundan <juha.lundan@frostwoods.cc>
2014-04-09 19:17:11 +0300

    Updated logging level

--- src/main/java/fi/itsfactory/siri/vm/uploader/listener/StatisticsListener.java
+++ src/main/java/fi/itsfactory/siri/vm/uploader/listener/StatisticsListener.java
@@ -2,17 +2,15 @@ package fi.itsfactory.siri.vm.uploader.listener;
 
 import java.util.logging.Logger;
 
-
 public class StatisticsListener implements ResponseListener {
 	private static Logger logger = Logger.getLogger(StatisticsListener.class.getName());
 	@Override
 	public void handleResponse(ListenerResponse response) {
-		logger.fine("Received response with " + response.getResponse().length() + " characters in "
+		logger.info("Received response with " + response.getResponse().length() + " characters in "
 				+ (response.getResponseTime().longValue()) + " ms.");
 	}
 
 	@Override
 	public void handleException(Exception exception) {
 	}
-
 }
